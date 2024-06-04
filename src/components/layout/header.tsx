551798d03fa75d38223eaf6d65dfa612981f3a71
Natalia Justicia <nataliajusticia@gmail.com>
2024-06-04 15:35:09 +0200

    feat: Add Netnode logo to header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,13 +1,21 @@
-import { Logo } from '@/lib/icons';
+import { Logo, NetnodeLogo } from '@/lib/icons';
 import Link from 'next/link';
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 border-b border-b-slate-200 shrink-0 bg-white">
-      <div className="max-container w-full">
+      <div className="max-container w-full flex justify-between items-center">
         <Link href="/" className="flex gap-2 font-bold">
           <Logo />
           my-ai-assistant
+        </Link>
+
+        <Link
+          href="https://www.netnode.ch"
+          className="flex gap-2 font-bold"
+          target="_blank"
+        >
+          <NetnodeLogo className="w-8 h-8" />
         </Link>
       </div>
     </header>
--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -1,5 +1,6 @@
-// Site logo
+// Logos
 export { default as Logo } from '/public/icons/terminal.svg';
+export { default as NetnodeLogo } from '/public/netnode.svg';
 
 export { default as BotIcon } from '/public/icons/bot.svg';
 export { default as UserIcon } from '/public/icons/user.svg';
